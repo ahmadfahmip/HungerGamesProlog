b585cd1ee9c66a677c3a99239c50b442af96c137
Adylan Roaffa Ilmy <adylanrff@gmail.com>
2017-11-13 14:19:33 +0700

    premerge

--- main.pl
+++ main.pl
@@ -1,4 +1,14 @@
 start:-
+  init_player,
   repeat,
   read(X),
   call(X).
+
+
+stats:-
+  get_health(Health),
+  write('Health :'),
+  write(Health),nl,
+  write('Hunger :'),
+  get_hunger(Hunger),
+  write(Hunger),nl.
--- player.pl
+++ player.pl
@@ -1,4 +1,4 @@
-:- dynamic(player/6).
+:- dynamic(player/5).
 
 default_health(100).
 default_hunger(20).
@@ -6,82 +6,82 @@ default_thirst(50).
 default_weapon('nothing').
 default_item_list([]).
 
-init_player(ID):-
+init_player:-
   default_health(Health),
   default_hunger(Hunger),
   default_thirst(Thirst),
   default_weapon(Weapon),
   default_item_list(ItemList),
   % FYI, assert buat masukin ke database
-  asserta(player(ID,Health,Hunger,Thirst,Weapon,ItemList)).
+  asserta(player(Health,Hunger,Thirst,Weapon,ItemList)).
 
 % Health
-increase_health(ID, Amount):-
-  retract(player(ID,Health,Hunger,Thirst,Weapon,ItemList)),
+increase_health( Amount):-
+  retract(player(Health,Hunger,Thirst,Weapon,ItemList)),
   ResultHealth is Health+Amount,
-  asserta(player(ID,ResultHealth,Hunger,Thirst,Weapon,ItemList)).
+  asserta(player(ResultHealth,Hunger,Thirst,Weapon,ItemList)).
 
-decrease_health(ID,Amount):-
-  retract(player(ID,Health,Hunger,Thirst,Weapon,ItemList)),
+decrease_health(Amount):-
+  retract(player(Health,Hunger,Thirst,Weapon,ItemList)),
   ResultHealth is Health-Amount,
-  asserta(player(ID,ResultHealth,Hunger,Thirst,Weapon,ItemList)).
+  asserta(player(ResultHealth,Hunger,Thirst,Weapon,ItemList)).
 
-get_health(ID,Health):-
-  player(ID,Health,_,_,_,_).
+get_health(Health):-
+  player(Health,_,_,_,_).
 
-set_health(ID,Health):-
-  retract(player(ID,CurrentHealth,Hunger,Thirst,Weapon,ItemList)),
-  asserta(player(ID,Health,Hunger,Thirst,Weapon,ItemList)).
+set_health(Health):-
+  retract(player(CurrentHealth,Hunger,Thirst,Weapon,ItemList)),
+  asserta(player(Health,Hunger,Thirst,Weapon,ItemList)).
 
 % Hunger
-increase_hunger(ID, Amount):-
-  retract(player(ID,Health,Hunger,Thirst,Weapon,ItemList)),
+increase_hunger( Amount):-
+  retract(player(Health,Hunger,Thirst,Weapon,ItemList)),
   ResultHunger is Hunger+Amount,
-  asserta(player(ID,Health,ResultHunger,Thirst,Weapon,ItemList)).
+  asserta(player(Health,ResultHunger,Thirst,Weapon,ItemList)).
 
-decrease_hunger(ID,Amount):-
-  retract(player(ID,Health,Hunger,Thirst,Weapon,ItemList)),
+decrease_hunger(Amount):-
+  retract(player(Health,Hunger,Thirst,Weapon,ItemList)),
   ResultHunger is Hunger-Amount,
-  asserta(player(ID,Health,ResultHunger,Thirst,Weapon,ItemList)).
+  asserta(player(Health,ResultHunger,Thirst,Weapon,ItemList)).
 
-get_hunger(ID,Hunger):-
-  player(ID,_,Hunger,_,_,_).
+get_hunger(Hunger):-
+  player(_,Hunger,_,_,_).
 
-set_hunger(ID,Hunger):-
-  retract(player(ID,Health,CurrentHunger,Thirst,Weapon,ItemList)),
-  asserta(player(ID,Health,Hunger,Thirst,Weapon,ItemList)).
+set_hunger(Hunger):-
+  retract(player(Health,CurrentHunger,Thirst,Weapon,ItemList)),
+  asserta(player(Health,Hunger,Thirst,Weapon,ItemList)).
 
 % Thirst
-increase_thirst(ID,Amount):-
-  retract(player(ID,Health,Hunger,Thirst,Weapon,ItemList)),
+increase_thirst(Amount):-
+  retract(player(Health,Hunger,Thirst,Weapon,ItemList)),
   ResultThirst is Thirst+Amount,
-  asserta(player(ID,Health,Hunger,ResultThirst,Weapon,ItemList)).
+  asserta(player(Health,Hunger,ResultThirst,Weapon,ItemList)).
 
-decrease_thirst(ID,Amount):-
-  retract(player(ID,Health,Hunger,Thirst,Weapon,ItemList)),
+decrease_thirst(Amount):-
+  retract(player(Health,Hunger,Thirst,Weapon,ItemList)),
   ResultThirst is Thirst-Amount,
-  asserta(player(ID,Health,Hunger,ResultThirst,Weapon,ItemList)).
+  asserta(player(Health,Hunger,ResultThirst,Weapon,ItemList)).
 
-get_thirst(ID,Thirst):-
-  player(ID,_,_,Thirst,_,_).
+get_thirst(Thirst):-
+  player(_,_,Thirst,_,_).
 
-set_thirst(ID,Thirst):-
-  retract(player(ID,Health,Hunger,CurrentThirst,Weapon,ItemList)),
-  asserta(player(ID,Health,Hunger,Thirst,Weapon,ItemList)).
+set_thirst(Thirst):-
+  retract(player(Health,Hunger,CurrentThirst,Weapon,ItemList)),
+  asserta(player(Health,Hunger,Thirst,Weapon,ItemList)).
 
 % Weapon
-set_weapon(ID,Weapon):-
-  retract(player(ID,Health,Hunger,Thirst,CurrentWeapon,ItemList)),
-  asserta(player(ID,Health,Hunger,Thirst,Weapon,ItemList)).
+set_weapon(Weapon):-
+  retract(player(Health,Hunger,Thirst,CurrentWeapon,ItemList)),
+  asserta(player(Health,Hunger,Thirst,Weapon,ItemList)).
 
-get_weapon(ID,Weapon):-
-  player(ID,_,_,_,Weapon,_).
+get_weapon(Weapon):-
+  player(_,_,_,Weapon,_).
 
 % Item List
-add_item(ID,Item):-
-  retract(player(ID,Health,Hunger,Thirst,Weapon,ItemList)),
+add_item(Item):-
+  retract(player(Health,Hunger,Thirst,Weapon,ItemList)),
   append([Item],ItemList,NewItemList),
-  asserta(player(ID,Health,Hunger,Thirst,Weapon,NewItemList)).
+  asserta(player(Health,Hunger,Thirst,Weapon,NewItemList)).
 
-get_item_list(ID,ItemList):-
-  player(ID,_,_,_,_,ItemList).
+get_item_list(ItemList):-
+  player(_,_,_,_,ItemList).
