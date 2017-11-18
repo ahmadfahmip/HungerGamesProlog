6c587eb3a51b43afc3b93cc0f41595a85f12cea4
Phroton98 <joseph_salimin@hotmail.com>
2017-11-18 19:03:26 +0700

    pls

--- main.pl
+++ main.pl
@@ -1,7 +1,6 @@
 /* Hentikan semua kegilaan ini */
 
-/* This command to start the game */
-
+/* This is command to start the game */
 start :- g_read(started, X), X = 1, write('Game has already started'), nl, fail, !.
 start :-
 	g_read(started, X), X = 0, !,
@@ -14,20 +13,29 @@ start :-
 	welcome,
 	main_loop.
 
-/* init everything when game started without load */
+/* Main loop of the program */
+main_loop :-
+	repeat,
+	write('Do something > '),
+	read(Input),
+	format('You said: ~w', [Input]), nl,
+	call(Input), is_turn(Input),
+	Input = quit, !.
+
+/* Init everything when game started without load */
 init_everything :-
 	init_every_item,
 	init_player.
 
-main_loop :-
-	repeat,
-	write('Do something > '),
-	read(X),
-	format('You said: ~w', [X]), nl,
-	call(X),
-	X = quit, !.
-
+/* Check if user want to load from save file */
 check_load(0) :- init_everything, !.
 check_load(1) :- !.
 
-
+/* Check for command which not make a turn */
+is_turn(info) :- !.
+is_turn(save) :- !.
+is_turn(status) :- !.
+is_turn(look) :- !.
+is_turn(X) :- 
+	decrease_hunger(2),
+	decrease_thirst(2).
