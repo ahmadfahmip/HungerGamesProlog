1b7d42d447b4c5dc7f0046e2ab5a2b9565f71d51
Phroton98 <joseph_salimin@hotmail.com>
2017-11-18 18:39:16 +0700

    pls

--- main.pl
+++ main.pl
@@ -5,13 +5,16 @@
 start :- g_read(started, X), X = 1, write('Game has already started'), nl, fail, !.
 start :-
 	g_read(started, X), X = 0, !,
-	welcome,
 	g_assign(started, 1),
 	set_seed(50), randomize,
-	init_everything,
+	repeat,
+	write('Do you want to load save file or want to start from scratch?? (Press 1 for yes or 0 for no)'), 
+	nl, write('> '),
+	read(X), check_load(X),
+	welcome,
 	main_loop.
 
-/* init everythin when game started without load */
+/* init everything when game started without load */
 init_everything :-
 	init_every_item,
 	init_player.
@@ -23,4 +26,8 @@ main_loop :-
 	format('You said: ~w', [X]), nl,
 	call(X),
 	X = quit, !.
+
+check_load(0) :- init_everything, !.
+check_load(1) :- !.
+
 
